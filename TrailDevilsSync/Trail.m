0ad335ebcc7415e1cbb310ff3241fbf7c7ac6368
Alexandre Joly <alexandre.joly@mekanics.ch>
2011-10-06 14:20:04 +0200

    Trail managed object created

---
+++ TrailDevilsSync/Trail.m
@@ -0,0 +1,33 @@
+//
+//  Trail.m
+//  TrailDevilsSync
+//
+//  Created by Alexandre Joly on 06.10.11.
+//  Copyright (c) 2011 HSR. All rights reserved.
+//
+
+#import "Trail.h"
+
+
+@implementation Trail
+
+@dynamic country;
+@dynamic countryId;
+@dynamic createdDate;
+@dynamic desc;
+@dynamic favorits;
+@dynamic gmapX;
+@dynamic gmapY;
+@dynamic imageUrl120;
+@dynamic imageUrl800;
+@dynamic info;
+@dynamic isCommercial;
+@dynamic isOpen;
+@dynamic journey;
+@dynamic name;
+@dynamic nextCity;
+@dynamic state;
+@dynamic trailId;
+@dynamic url;
+
+@end
