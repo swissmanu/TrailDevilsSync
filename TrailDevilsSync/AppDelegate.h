afcedef16a378dcb4bc531d17a71b791c9e53b8a
Manuel Alabor <msites@msites.net>
2011-10-06 12:36:20 +0200

    added xcode project

---
+++ TrailDevilsSync/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TrailDevilsSync
+//
+//  Created by Manuel Alabor on 06.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ TrailDevilsSync/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrailDevilsSync
+//
+//  Created by Manuel Alabor on 06.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
