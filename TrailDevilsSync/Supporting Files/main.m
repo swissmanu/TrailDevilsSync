ba9151721fb213ce4e7201c7e00a73006066f428
Alexandre Joly <alexandre.joly@mekanics.ch>
2011-11-19 14:17:30 +0100

    file structure refactored

---
+++ TrailDevilsSync/Supporting Files/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrailDevilsSync
+//
+//  Created by Manuel Alabor on 07.10.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDSAppDelegate class]));
+    }
+}
